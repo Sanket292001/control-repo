ddccdf2a53d49575b3cf9e2b139f4100c5da135b
Sanket Salunke <52500703+Sanket292001@users.noreply.github.com>
2024-03-22 11:04:44 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,8 @@
 node default {
-  file { '/root/readme':
-    ensure => file
-  }
+}
+
+node instance-20240322-034023.us-central1-a.c.clgcporg8-047.internal {
+  include role::master
 }
 
 node instance-20240322-sdasd.us-central1-a.c.clgcporg8-047.internal {
