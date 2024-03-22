d26b3f93fb97f99fa80af2a280d28e5aa60a4b48
Sanket Salunke <52500703+Sanket292001@users.noreply.github.com>
2024-03-22 14:53:55 +0530

    Update web.pp

--- site/profile/manifests/web.pp
+++ site/profile/manifests/web.pp
@@ -1,8 +1,8 @@
 class profile::web {
   include nginx
 
-  file { '/usr/share/nginx/html/index.html':
-    ensure => present,
-    content => '<h1>Hello..</h1> <h3>Configured by Puppet</h3>'
-  }
+  #file { '/usr/share/nginx/html/index.html':
+  #  ensure => present,
+  #  content => '<h1>Hello..</h1> <h3>Configured by Puppet</h3>'
+  #}
 }
