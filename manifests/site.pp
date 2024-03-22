2d6072920b50268fa29a6c86db9fad4c7e23d78e
Sanket Salunke <52500703+Sanket292001@users.noreply.github.com>
2024-03-22 11:10:06 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,10 @@
 node default {
 }
 
-node instance-20240322-034023.us-central1-a.c.clgcporg8-047.internal {
+node 'instance-20240322-034023.us-central1-a.c.clgcporg8-047.internal' {
   include role::master
 }
 
-node instance-20240322-sdasd.us-central1-a.c.clgcporg8-047.internal {
+node 'instance-20240322-sdasd.us-central1-a.c.clgcporg8-047.internal' {
   include role::web_server
 }
